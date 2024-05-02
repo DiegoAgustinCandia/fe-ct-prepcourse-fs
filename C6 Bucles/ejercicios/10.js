dfaadb73728fc2e5af351029b504b355995aadf0
DiegoAgustinCandia <politicomisionero@hotmail.com>
2024-05-02 19:32:37 -0300

    C6

--- C6 Bucles/ejercicios/10.js
+++ C6 Bucles/ejercicios/10.js
@@ -1,7 +1,16 @@
+
 function esDiezOCinco(num) {
   // Retornar true si "num" es 10 o 5.
   // De lo contrario, retornar false.
   // Tu código:
+  while (num !== 10 && num !== 5) {
+    return false;
+  }
+  return true;
 }
 
 module.exports = esDiezOCinco;
+
+
+
+
