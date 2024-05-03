bef488a2c07ffc83b5d6215d6e341b3c648bc621
DiegoAgustinCandia <politicomisionero@hotmail.com>
2024-05-02 23:04:09 -0300

    C6

--- C6 Bucles/ejercicios/16.js
+++ C6 Bucles/ejercicios/16.js
@@ -5,6 +5,18 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!".
   // Si "idioma" no es ninguno de los anteriores o es `undefined`, devuelve "Hola!".
   // Tu código:
+  if (idioma === "aleman") {
+    return "Guten Tag!";
+  } else if (idioma === "mandarin") {
+    return "Ni Hao!";
+  } else if (idioma === "ingles") {
+    return "Hello!";
+  } else if (idioma !== "ingles" || idioma !== "mandarin" || idioma !== "aleman" || idioma === undefined) {
+    return "Hola!";
+  }
+
+
+
 }
 
 module.exports = saludo;
