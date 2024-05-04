e406f704d0315f2f89fc3336609f88c833818c62
DiegoAgustinCandia <politicomisionero@hotmail.com>
2024-05-04 20:46:06 -0300

    C6

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,22 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  let start = Math.min(a, b);
+  let end = Math.max(a, b);
+  let product = 1; // Inicializa el producto en 1, ya que cualquier número multiplicado por 1 es igual a sí mismo
+
+  for (let i = start; i <= end; i++) {
+    product *= i; // Multiplica cada número por el producto acumulado
+  }
+
+  return product; // Devuelve el producto acumulado al finalizar el bucle
 }
 
 module.exports = productoEntreNúmeros;
+
+
+
+
+
+
+
