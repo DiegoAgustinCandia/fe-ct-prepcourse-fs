1392d40adafae5b6d5d9a82c3ef6c98a7958d5eb
DiegoAgustinCandia <politicomisionero@hotmail.com>
2024-05-05 00:13:24 -0300

    C6

--- C6 Bucles/ejercicios/22.js
+++ C6 Bucles/ejercicios/22.js
@@ -3,6 +3,14 @@ function doWhile(num) {
   // Retorna el valor final.
   // PISTA: Utiliza el bucle do-while.
   // Tu código:
+  let veces = 0; // Inicializamos una variable para contar las veces que hemos incrementado num
+
+  do {
+    num += 5; // Aumentamos num en 5 en cada iteración
+    veces++; // Incrementamos el contador de veces
+  } while (veces < 8); // Repetimos el bucle mientras no hayamos llegado a 8 veces
+
+  return num; // Devolvemos el valor final de num
 }
 
 module.exports = doWhile;
--- C6 Bucles/ejercicios/23.js
+++ C6 Bucles/ejercicios/23.js
@@ -6,6 +6,17 @@ function esNumeroPrimo(numero) {
   // IMPORTANTE: Recuerda que un número primo es aquel que
   // solo es divisible por sí mismo y por 1.
   // Tu código:
+  if (numero <= 1) { // Los números menores o iguales a 1 no son primos
+    return false;
+  }
+
+  for (let i = 2; i < numero; i++) { // Comenzamos desde 2 y recorremos hasta el número anterior a "numero"
+    if (numero % i === 0) { // Si "numero" es divisible por algún número diferente de sí mismo y de 1
+      return false; // No es primo
+    }
+  }
+
+  return true; // Si no se encontró ningún divisor diferente de sí mismo y de 1, entonces es primo
 }
 
 module.exports = esNumeroPrimo;
