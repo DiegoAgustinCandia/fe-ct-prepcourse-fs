80bdb5a0cbb77bd692856c2843b47236aa6fc07a
DiegoAgustinCandia <politicomisionero@hotmail.com>
2024-05-05 00:05:30 -0300

    C6

--- C6 Bucles/ejercicios/20.js
+++ C6 Bucles/ejercicios/20.js
@@ -1,8 +1,20 @@
 function sumarHastaNConBreak(n) {
-   // La función recibe un numero n por argumento.
+   // La función recibe un número n por argumento.
    // Devuelve la suma de todos los números desde 1 hasta n.
    // Si la suma supera a 100, detén el bucle usando break.
    // Tu código:
+
+   let suma = 0; // Inicializa la variable suma para acumular la suma de los números
+
+   for (let i = 1; i <= n; i++) {
+      suma += i; // Suma el valor de i a la suma actual
+
+      if (suma > 100) { // Verifica si la suma supera 100
+         break; // Detiene el bucle si la suma supera 100
+      }
+   }
+
+   return suma; // Devuelve la suma acumulada
 }
 
 module.exports = sumarHastaNConBreak;
--- C6 Bucles/ejercicios/21.js
+++ C6 Bucles/ejercicios/21.js
@@ -1,9 +1,24 @@
+
 function esPotenciaDeDos(numero) {
   // La función recibe un "numero" por argumento.
   // Determina si es una potencia de 2.
   // Devuelve true si lo es, sino devuelve false.
   // PISTA: Utiliza un bucle while.
   // Tu código:
+
+
+  while (numero > 1) { // Mientras el número sea mayor que 1
+    if (numero % 2 !== 0) { // Si el número no es divisible por 2
+      return false; // No es una potencia de 2
+    }
+    numero = numero / 2; // Divide el número por 2
+  }
+
+  return true; // Si el bucle termina y el número es 1, entonces es una potencia de 2
 }
 
 module.exports = esPotenciaDeDos;
+
+
+
+
